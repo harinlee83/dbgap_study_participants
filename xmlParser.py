ff4f6f12584d6e06c4fb9f64f87248535924a975
Harin Lee <harinlee83@gmail.com>
2022-06-08 09:28:36 -0400

    commented xmlParser.py

--- xmlParser.py
+++ xmlParser.py
@@ -2,37 +2,41 @@ import glob
 import csv
 import xml.etree.ElementTree as ET
 
+# Folder with files to iterate through
 folderPath = './xml'
-# Grab list of all the files in folder
+
+# Return list of all the files in folder
 filePaths = list(glob.glob(folderPath + "/*"))
 
 # Reorder the list of filenames
 myList = []
 orderedList = []
 
+# Extract phs numbers from all the files so they can be sorted
 for file in filePaths:
     file = file.replace("./xml/","")
     file = file.replace(".xml","")
     myList.append(int(file))
 
-# Sort the list
+# Sort the list of integers
 myList.sort()
 
+# Add extension and path information
 for file in myList:
     # Pad numbers with leading zeros
     file = str(file).zfill(6)
     orderedList.append("./xml/" + file + ".xml")
 
-# Output csv files with phs and num_participants
+# Output csv file
 csvFile = "./phsNumParticipants.csv"
 
 with open(csvFile, "w") as writeFile:
     fieldnames = ["filename","phs","num_participants"]
     writer = csv.DictWriter(writeFile, fieldnames=fieldnames)
 
-    # Store the column headers and print them in newFile
     writer.writeheader()
 
+    # Parse phs and num_participants from each file
     for file in orderedList:
         mytree = ET.parse(file)
         myroot = mytree.getroot()
